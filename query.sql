678c2fa036c441d00e1f3b414d2f1ae6245ddb23
ruzwn <ruzwn@users.noreply.github.com>
2022-07-01 00:38:23 +0300

    Add sql query task.

---
+++ query.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE Products (
+    Id INT PRIMARY KEY,
+    Name VARCHAR(100)
+);
+
+CREATE TABLE Categories (
+    Id INT PRIMARY KEY,
+    Name VARCHAR(100)
+);
+
+CREATE TABLE ProductsCategories (
+    ProductId INT REFERENCES Products(Id),
+    CategoryId INT REFERENCES Categories(Id),
+    PRIMARY KEY (ProductId, CategoryId)
+);
+
+SELECT P.Name, C.Name
+FROM Products P
+LEFT JOIN ProductsCategories PC
+    on P.Id = PC.ProductId
+LEFT JOIN Categories C
+    on PC.CategoryId = C.Id;
